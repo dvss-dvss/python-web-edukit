6692a5cad336c404c80f8c1aeb3299236f8be375
dvss-dvss <kosevojdavid3@gmail.com>
2026-03-18 18:57:19 +0100

    Add course management views and templates for creating, editing, and deleting courses

--- core/urls.py
+++ core/urls.py
@@ -18,13 +18,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import include, path
+
 from courses.views import HomeView
 
 urlpatterns = [
-    path('account/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
+    path('course/', include('courses.urls')),
     path('', HomeView.as_view(), name='home'),
 ]
 
