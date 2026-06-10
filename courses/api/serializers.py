a04476a0cb033912a8486363209fe5f94753baca
dvss-dvss <kosevojdavid3@gmail.com>
2026-06-10 18:59:01 +0200

    Add CourseWithContentsSerializer and update CourseViewSet: implement content retrieval for courses and enhance serializers

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from courses.models import Module, Subject
+from courses.models import Module, Subject, Course, Content
 
 class ModuleSerializer(serializers.ModelSerializer):
 
@@ -12,7 +12,7 @@ class CourseSerializer(serializers.ModelSerializer):
     modules = ModuleSerializer(many=True, read_only=True)
 
     class Meta:
-        model = Subject
+        model = Course
         fields = ['id', 'subject', 'title', 'slug', 'overview', 'created', 'owner', 'modules']
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -20,3 +20,30 @@ class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ['id', 'title', 'slug']
+
+class ItemRelatedField(serializers.RelatedField):
+
+    def to_representation(self, value):
+        return value.render()
+    
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRelatedField(read_only=True)
+
+    class Meta:
+        model = Content
+        fields = ['order', 'item']
+
+class ModuleWithContentsSerializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True)
+
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description', 'contents']
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+    modules = ModuleWithContentsSerializer(many=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug', 'overview', 'created', 'owner', 'modules']
+        
